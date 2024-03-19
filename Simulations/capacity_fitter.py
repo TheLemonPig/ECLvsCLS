f7fd5a3425e46cc42121995c121f4178d7cb081d
TheLemonPig <joshua_hewson@brown.edu>
2024-03-19 17:45:23 -0400

    Add functionality to fit sample sizes and model sizes to the random variability in datasets such that knowledge of sufficient capacity is maintained across repetitions

--- Simulations/capacity_fitter.py
+++ Simulations/capacity_fitter.py
@@ -8,14 +8,13 @@ import os
 
 from Models.associator import Associator, Trainer
 from Models.transforms import rotate
-
 from utils import make_data
 
 # Define input, hidden, and output sizes
 config = dict({
     'input_size': 5,
     'output_size': 5,
-    'sample_size': 50,
+    'hidden_size': 10,
     'model_layers': 2,
     # Extra parameters
     'first_epochs': -1,
@@ -30,23 +29,23 @@ config = dict({
     'optimizer': optim.Adam
 })
 config['seeds'] = list(range(config['n_reps']))
-start_size = 12
+start_size = 30
 seed_capacities = dict({k: -1 for k in range(config['n_reps'])})
 for seed in config['seeds']:
-    print(f'Finding Sufficient Capacity for seed={seed}')
+    print(f'Finding Number of Samples that meet the model\'s Sufficient Capacity for seed={seed}')
     seed_capacity = dict()
-    hidden_size = start_size
+    sample_size = start_size
     while (True not in seed_capacity.values()) or (False not in seed_capacity.values()):
-        if len(seed_capacity) > 0 and seed_capacity[hidden_size] is True:
-            print(f'Sufficient Capacity <= {hidden_size}')
-            hidden_size -= 1
-        elif len(seed_capacity) > 0 and seed_capacity[hidden_size] is False:
-            print(f'Sufficient Capacity > {hidden_size}')
-            hidden_size += 1
+        if len(seed_capacity) > 0 and seed_capacity[sample_size] is True:
+            print(f'Sufficient Data Capacity >= {sample_size}')
+            sample_size += 1
+        elif len(seed_capacity) > 0 and seed_capacity[sample_size] is False:
+            print(f'Sufficient Data Capacity < {sample_size}')
+            sample_size -= 1
 
         torch.manual_seed(seed)
         # Create an instance of the VectorMapper class
-        model = Associator(config['input_size'], hidden_size, config['output_size'])
+        model = Associator(config['input_size'], config['hidden_size'], config['output_size'])
 
         # Define a loss function and an optimizer
         criterion = config['criterion']()
@@ -56,29 +55,29 @@ for seed in config['seeds']:
         trainer = Trainer(model, criterion, optimizer)
 
         # Random x data
-        x = make_data(config['sample_size'], config['input_size'], seed)
+        x = make_data(sample_size, config['input_size'], seed)
 
         # Rotated and noise-embedded y data
         y = (1.0 - config['noise']) * rotate(x, n_dims=int(config['signal_complexity'])) + \
-            config['noise'] * make_data(config['sample_size'], config['input_size'], seed+1)
+            config['noise'] * make_data(sample_size, config['input_size'], -seed)
 
         results = trainer.train((x, y, 0), num_epochs=config['first_epochs'], stops=config['stops'])
-        seed_capacity[hidden_size] = (results['train_accuracy'][-1] == 1.0)
+        seed_capacity[sample_size] = (results['train_accuracy'][-1] == 1.0)
     sufficient_capacity = start_size
-    for hidden_size in seed_capacity.keys():
-        if seed_capacity[hidden_size]:
+    for sample_size in seed_capacity.keys():
+        if seed_capacity[sample_size]:
             if seed_capacity[sufficient_capacity]:
-                if hidden_size < sufficient_capacity:
-                    sufficient_capacity = hidden_size
+                if sample_size > sufficient_capacity:
+                    sufficient_capacity = sample_size
             else:
-                sufficient_capacity = hidden_size
+                sufficient_capacity = sample_size
     print(f'Sufficient Capacity = {sufficient_capacity}\n')
     seed_capacities[seed] = sufficient_capacity
 
-config['model_seed_capacities'] = seed_capacities
+config['data_seed_capacities'] = seed_capacities
 print(seed_capacities)
 
-filename = 'Capacity_Estimator_' + str(datetime.now()) + '.pkl'
+filename = 'Capacity_Fitter_' + str(datetime.now()) + '.pkl'
 filepath = os.path.join('../Logs', filename)
 with open(filepath, 'wb') as f:
     pickle.dump(config, f)
